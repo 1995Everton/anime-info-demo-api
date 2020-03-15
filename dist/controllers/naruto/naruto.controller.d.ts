170faebfa9cdee97d77106c4a1aad86dcb0d243b
1995Everton <everton_cardoso@live.com>
2020-03-15 18:21:58 -0300

    :recycle: remove dist e node_modules

--- dist/controllers/naruto/naruto.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { NarutoService } from './naruto.service';
-import { Response } from 'express';
-export declare class NarutoController {
-    private narutoService;
-    constructor(narutoService: NarutoService);
-    getCharacter(name: string, response: Response): Promise<any>;
-}
--- dist/controllers/naruto/naruto.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Naruto } from 'anime-info';
-import { Info } from 'anime-info/lib/app/naruto/models';
-export declare class NarutoService {
-    naruto: Naruto;
-    constructor();
-    getCharacter(name: string): Promise<Info>;
-}
