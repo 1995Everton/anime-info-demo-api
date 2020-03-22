c9e6588c5aa7a8fcd07c142a81514a0e3a955b27
1995Everton <everton_cardoso@live.com>
2020-03-21 22:09:04 -0300

    :ok_hand: atualizada versao do anime-info

--- src/controllers/naruto/naruto.service.ts
+++ src/controllers/naruto/naruto.service.ts
@@ -1,17 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { Naruto } from 'anime-info'
-import { Info } from 'anime-info/lib/app/naruto/models';
+import { Injectable } from "@nestjs/common";
+import { Naruto } from "anime-info";
+import { Info } from "anime-info/dist/naruto/models";
 @Injectable()
 export class NarutoService {
+  naruto: Naruto;
 
-    naruto: Naruto;
+  constructor() {
+    this.naruto = new Naruto();
+  }
 
-    constructor(){
-        this.naruto = new Naruto()
-    }
-    
-    public async getCharacter(name: string): Promise<Info>{
-        return await this.naruto.getCharacter(name);
-        
-    }
+  public async getCharacter(name: string): Promise<Info> {
+    return await this.naruto.getCharacter(name);
+  }
 }
